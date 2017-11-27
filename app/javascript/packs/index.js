12224fd4e95a7d8e5b5fecd86eabf7ffa02c1e1c
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2017-11-27 13:44:56 -0500

    First commit

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from '../react/src/Home';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Home />, document.getElementById('app'));
+})
---
+++ app/javascript/react/src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = props => {
+  return(
+    <h1 className='home-test'>Hopefully there will be some cool d3 charts here one day</h1>
+  )
+}
+
+export default Home
